c1acd359164db9152eb6818e8bde5ab9428845af
Raj Sudharshan Vidyalatha Natarajan <sudharshann05@gmail.com>
2025-06-26 20:42:06 -0700

    Updated next.config.ts

--- sirpi-google-frontend/next.config.ts
+++ sirpi-google-frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["*"],
+  experimental: {
+    turbo: false, // disables turbopack
+  },
 };
 
 export default nextConfig;
